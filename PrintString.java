d366703c2820faa79ef200fad62c4130aff20661
Tamilgithub1 <31219547+Tamilgithub1@users.noreply.github.com>
2018-08-24 14:08:00 +0530

    Create PrintString.java

---
+++ PrintString.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class te {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	String s=in.next();
+	  int n=in.nextInt();
+	  if(n<=s.length())
+	  System.out.println(s.substring(n));
+}}
